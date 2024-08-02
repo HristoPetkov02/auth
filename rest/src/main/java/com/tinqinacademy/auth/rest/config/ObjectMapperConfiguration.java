94dd2adf039476cda4664ebdf5250bd125ebe290
LlamaLamap1 <hristo.petkov221@gmail.com>
2024-08-02 11:43:47 +0300

    Added the object mapper configuration

---
+++ rest/src/main/java/com/tinqinacademy/auth/rest/config/ObjectMapperConfiguration.java
@@ -0,0 +1,21 @@
+package com.tinqinacademy.auth.rest.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfiguration {
+   @Bean
+   public ObjectMapper objectMapper(){
+       ObjectMapper mapper = new ObjectMapper();
+       mapper.registerModule(new JavaTimeModule());
+       mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+       mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+       mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+       return mapper;
+   }
+}
